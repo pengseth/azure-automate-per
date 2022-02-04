a42cf5f858b04c8e52a03c7f351d167ac3e1b54b
pengseth <pearen64@gmail.com>
2022-02-04 16:09:25 +0100

    commit

--- src/oppgave_3.ps1
+++ src/oppgave_3.ps1
@@ -1,13 +1,6 @@
-# Feilhåndtering - stopp programmet hvis det dukker opp noen feil
-# Se https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_preference_variables?view=powershell-7.2#erroractionpreference
-$ErrorActionPreference = 'Stop'
-
 $Url ="https://nav-deckofcards.herokuapp.com/shuffle"
-
 $response = Invoke-WebRequest -Uri $Url
-
 $cards = $response.Content | ConvertFrom-Json
-
 $kortstokk = @()
 #$cards.GetType()
 foreach ($card in $cards) {
