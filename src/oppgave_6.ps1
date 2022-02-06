f94c807769e4927a2e36d96c5dfefd030b66a134
pengseth <pearen64@gmail.com>
2022-02-06 14:19:59 +0100

    oppgave_6

---
+++ src/oppgave_6.ps1
@@ -0,0 +1,74 @@
+#Oppgave_6
+param (
+    [Parameter()]
+    [string]
+    $Urlkortstokk = "https://nav-deckofcards.herokuapp.com/shuffle"
+)
+$ErrorActionPreference = 'Stop'
+
+$response = Invoke-WebRequest -Uri $Urlkortstokk
+$cards = $response.Content | ConvertFrom-Json
+
+# $sum = 0
+# foreach ($card in $cards) {
+#     $sum += switch ($card.value){
+#         'J' { 10 }
+#         'Q' { 10 }
+#         'K' { 10 }
+#         'A' { 11 }
+#         Default { $card.value }
+#     }
+# }
+foreach ($card in $cards) {
+    if($card.value -ceq 'J' -or $card.value -ceq 'Q' -or $card.value -ceq 'K'){
+        $poengKortstokk = $poengKortstokk + 10
+    }
+    elseif ( $card.value -ceq 'A') {
+        $poengKortstokk = $poengKortstokk + 11
+        
+    }
+}
+#Skriver ut Kortstokken
+function  kortstokkprint {
+    param (
+    [Parameter()]
+    [Object[]]
+        $cards
+    )
+    $kortstokk = @()
+#$cards.GetType()
+foreach ($card in $cards) {
+    $kortstokk = $kortstokk + ($card.suit[0]+$card.value)
+}
+    $kortstokk
+}
+$kortstokk = @()
+#$cards.GetType()
+foreach ($card in $cards) {
+    $kortstokk = $kortstokk + ($card.suit[0]+$card.value)
+}
+Write-Host "Kortstokk :$(kortstokkprint($cards))"
+Write-Host "Poengsum : $sum"
+
+$meg =  $cards[0..1]
+$cards = $cards[2..$cards.Length]
+
+$magnus = $cards[0..1]
+$cards = $cards[2..$cards.Length]
+
+Write-Host "meg: $(kortstokkprint($meg))"
+Write-Host "magnus: $(kortstokkprint($magnus))"
+Write-Host "kortstokk: $(kortstokkprint($cards))"
+
+
+#oppgave 6 start
+#mulig alternativ (ikke ferdig)
+# $meg = $kortstokk[0..1]
+# $kortstokk = $kortstokk[2..$kortstokk.Length]
+# $magnus = $kortstokk[3..4]
+# $kortstokk = $kortstokk[4..$kortstokk.Count]
+# $magnus = $kortstokk[0..1]
+# Write-Output "meg: $(kortStokkTilStreng -kortstokk $meg)"
+# $magnus = $kortstokk[0..1]
+# Write-Output "magnus: $(kortStokkTilStreng -kortstokk $magnus)"
+# $kortstokk = $kortstokk[2..$kortstokk.Count]
