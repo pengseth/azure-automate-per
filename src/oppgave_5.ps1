f9e956fca2027d39b5dc868227b446a78d850848
pengseth <pearen64@gmail.com>
2022-02-06 13:33:50 +0100

    oppgave 5

--- src/oppgave_5.ps1
+++ src/oppgave_5.ps1
@@ -1,4 +1,4 @@
-#Oppgave_4
+#Oppgave_5
 param (
     [Parameter()]
     [string]
@@ -8,9 +8,31 @@ $ErrorActionPreference = 'Stop'
 
 $response = Invoke-WebRequest -Uri $Urlkortstokk
 $cards = $response.Content | ConvertFrom-Json
+
+# $sum = 0
+# foreach ($card in $cards) {
+#     $sum += switch ($card.value){
+#         'J' { 10 }
+#         'Q' { 10 }
+#         'K' { 10 }
+#         'A' { 11 }
+#         Default { $card.value }
+#     }
+# }
+foreach ($card in $cards) {
+    if($card.value -ceq 'J' -or $card.value -ceq 'Q' -or $card.value -ceq 'K'){
+        $poengKortstokk = $poengKortstokk + 10
+    }
+    elseif ( $card.value -ceq 'A') {
+        $poengKortstokk = $poengKortstokk + 11
+        
+    }
+}
+#Skriver ut Kortstokken
 $kortstokk = @()
 #$cards.GetType()
 foreach ($card in $cards) {
     $kortstokk = $kortstokk + ($card.suit[0]+$card.value)
 }
 Write-Host "Kortstokk :$kortstokk"
+Write-Host "Poengsum : $sum"
