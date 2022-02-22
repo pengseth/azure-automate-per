8222bfec4f4c22d5b1f1e1c6fa845536a7277083
pengseth <pearen64@gmail.com>
2022-02-22 16:06:28 +0100

    oppgave_7.ps1

--- src/oppgave_7.ps1
+++ src/oppgave_7.ps1
@@ -7,17 +7,9 @@ param (
     $Urlkortstokk = "https://nav-deckofcards.herokuapp.com/shuffle"
 )
 #Stopp ved feil
-#$ErrorActionPreference = 'Stop'
 $ErrorActionPreference = 'SilentlyContinue'
-#try {
-    $webRequest = Invoke-WebRequest -Uri $Urlkortstokk
-    $kortstokk = $webRequest.Content 
-    $kortstokk = ConvertFrom-Json -InputObject $kortstokkJson
-#    }
-#catch {
-#    Write-Host "Fant ingen kort på "$Urlkortstokk
-#    Break
-#}
+#$ErrorActionPreference = [System.Management.Automations.ActionPreference]::SilentlyContinue
+
 $webRequest = Invoke-WebRequest -Uri $Urlkortstokk
 $kortstokk = $webRequest.Content | ConvertFrom-Json
 
@@ -59,14 +51,12 @@ function sumPoengKortstokk {
     $poengKortstokk = 0
 
 foreach ($kort in $kortstokk) {
-    if($kort.value -eq 'J' -or $kort.value -eq 'Q' -or $kort.value -eq 'K'){
-        $poengKortstokk = $poengKortstokk + 10
+    $poengKortstokk += switch ($kort.value) {
+        { $_ -cin @('J', 'Q', 'K') } { 10 }
+        'A' { 11 }
+        default { $kort.value }
     }
-    elseif ( $kort.value -eq 'A') {
-        $poengKortstokk = $poengKortstokk + 11
-        
-        }
-    }
+ }
     return $poengKortstokk
 }
 
@@ -85,41 +75,6 @@ Write-Output "magnus: $(kortstokkTilStreng -kortstokk $magnus)"
 $kortstokk = $kortstokk[2..$kortstokk.Count]
 Write-Output "Kortstokk: $(kortstokkTilStreng - kortstokk $kortstokk)"
 
-############################################################################## PAE Sjekk Her
-#Skriver ut Kortstokken
-#function  kortstokkprint {
- #   param (
- #   [Parameter()]
- #   [Object[]]
- #       $kortstokk
- #   )
-#  $kortstokk = @()
-#$cards.GetType()
-#foreach ($kort in $kortstokk) {
-#    $kortstokk = $kortstokk + ($kort.suit[0]+$kort.value)
-#}
-#    $kortstokk
-#}
-#$kortstokk = @()
-#$cards.GetType()
-#foreach ($kort in $kortstokk) {
-#    $kortstokk = $kortstokk + ($kort.suit[0]+$kort.value)
-#}
-#Write-Host "Kortstokk :$(kortstokkprint($kortstokk))"
-#Write-Host "Poengsum : $sum"
-#
-#$meg =  $kortstokk[0..1]
-#$kortstokk = $kortstokk[2..$kortstokk.Length]
-#
-#$magnus = $kortstokk[0..1]
-#$kortstokk = $kortstokk[2..$kortstokk.Length]
-#
-#Write-Host "meg: $(kortstokkprint($meg))"
-#Write-Host "magnus: $(kortstokkprint($magnus))"
-#Write-Host "kortstokk: $(kortstokkprint($kortstokk))"
-#
-# Ferdig oppgave 6
-# Start oppgave 7
 function skrivUtResultat {
     param (
         [String]
@@ -131,7 +86,7 @@ function skrivUtResultat {
         )
     Write-Output "Vinner: $vinner"
     Write-Output "magnus |$(sumpoengkortstokk -kortstokk $kortstokkmagnus) | $(kortstokktilstreng -kortstokk $kortstokkmagnus)"
-    Write-Output "meg |$(sumpoengkortstokk -kortstokk -kortstokkmeg) |  $(kortstokktilstreng -kortstokk $kortstokkmeg)"
+    Write-Output "meg |$(sumpoengkortstokk -kortstokk $kortstokkmeg)    |  $(kortstokktilstreng -kortstokk $kortstokkmeg)"
 }
 #Blackjack verdi er 21
 $blackjack = 21
@@ -139,11 +94,11 @@ $blackjack = 21
 if (((sumpoengkortstokk -kortstokk $meg) -eq $blackjack) -and ((sumpoengkortstokk -kortstokk $magnus) -eq $blackjack)) {
     skrivUtResultat -vinner "draw" -kortstokkmagnus $magnus -kortstokkmeg $meg
 }
-elseif ((sumpoengkortokk -kortstokk $meg) -eq $blackjack) {
+elseif ((sumpoengkortstokk -kortstokk $meg) -eq $blackjack) {
     skrivUtResultat -vinner "meg" -kortstokkmagnus $magnus -kortstokkmeg $meg
-    Exit
+    exit
 }
-elseif ((sumpoengkortokk -kortstokk $magnus) -eq $blackjack) {
+elseif ((sumpoengkortstokk -kortstokk $magnus) -eq $blackjack) {
    skrivUtResultat -vinner "magnus" -kortstokkmagnus $magnus -kortstokkmeg $meg
-   Exit
+   exit
 }
